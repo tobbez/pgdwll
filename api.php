9df5aeffe6cff33f35260cb23a8a0fc613fe0707
Torbjörn Lönnemark <tobbez@ryara.net>
2011-08-09 20:43:31 +0200

    add barebones api.php

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+
+$API_METHODS = array();
+
+
+/* args: `wanted` (optional, default 10) */
+$API_METHODS['retrieve'] = function($args) {
+};
+/* args: `text` (required) */
+$API_METHODS['add'] = function($args) {
+};
