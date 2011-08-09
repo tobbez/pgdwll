039138a9583e2984079ab6bef54c41334ceb26ed
Torbjörn Lönnemark <tobbez@ryara.net>
2011-08-09 21:01:13 +0200

    add method dispatcher to api

--- api.php
+++ api.php
@@ -1,7 +1,11 @@
 <?php
 
-$API_METHODS = array();
+function return_error($errmsg) {
+  echo json_encode(array('status' => 'fail', 'status_message' => $errmsg));
+  exit(0);
+}
 
+$API_METHODS = array();
 
 /* args: `wanted` (optional, default 10) */
 $API_METHODS['retrieve'] = function($args) {
@@ -9,3 +13,9 @@ $API_METHODS['retrieve'] = function($args) {
 /* args: `text` (required) */
 $API_METHODS['add'] = function($args) {
 };
+
+if (array_key_exists($_GET['method'], $API_METHODS)) {
+  $API_METHODS[$_GET['method']]($_POST);
+} else {
+  return_error("No such API method");
+}
